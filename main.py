b935727a7a857c42efe22123cdf280beabdbc132
Anand Rajan <63197676+QuantumMonkey@users.noreply.github.com>
2022-06-20 07:30:31 +0530

    Try 2

--- main.py
+++ main.py
@@ -5,22 +5,9 @@ With a massive dataset of over 12000 images, we will train a Convolutional Neura
 4 types of blood cells: Eosinophil, Lymphocyte, Monocyte, Neutrophil.
 """
 
-from keras.models import Sequential
 import tensorflow as tf
-# noinspection PyUnresolvedReferences
-import tensorflow_datasets as tfds
-tf.enable_eager_execution()
-from keras.layers.core import Dense, Activation, Dropout, Flatten
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.optimizers import SGD, RMSprop, adam_legacy as adam
-from keras.utils import np_utils
-from sklearn.tree import DecisionTreeClassifier
-import matplotlib.pyplot as plt
-import numpy as np
-import cv2
-import random
-from numpy import *
-from PIL import Image
-import theano
+print(tf.__version__)
 
-path_test = ""
+from keras.layers import Input, Lambda, Dense, Flatten, Conv2D, \
+    BatchNormalization, ZeroPadding2D, MaxPooling2D, Activation, add
+from keras.models import Model
