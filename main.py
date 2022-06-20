1c04715ff70b6d6bde70934ada42ab9c861342f9
Anand Rajan <63197676+QuantumMonkey@users.noreply.github.com>
2022-06-20 08:50:39 +0530

    Following from ACHMAD RIZKY RINO SAPUTRA Loaded image data, displayed a random image to check access

--- main.py
+++ main.py
@@ -6,8 +6,44 @@ With a massive dataset of over 12000 images, we will train a Convolutional Neura
 """
 
 import tensorflow as tf
-print(tf.__version__)
+#print(tf.__version__)
 
 from keras.layers import Input, Lambda, Dense, Flatten, Conv2D, \
     BatchNormalization, ZeroPadding2D, MaxPooling2D, Activation, add
 from keras.models import Model
+from keras.preprocessing import image
+from keras.utils.image_utils import load_img
+from keras.preprocessing.image import ImageDataGenerator
+
+from keras.optimizers import adam_v2 as Adam
+
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import matplotlib.pyplot as plt
+
+from glob import glob
+
+# Setting the converted image size
+IMG_SIZE = [200, 200]
+
+# Configuring training
+epochs = 10
+batch_size = 200
+
+# Load dataset
+train_path = "Blood Cell Dataset/dataset2-master/images/TRAIN"
+test_path = "Blood Cell Dataset/dataset2-master/images/TEST"
+
+# Find count of data
+train_files = glob(train_path + '/*/*.jp*g')
+test_files = glob(test_path + '/*/*.jp*g')
+
+# Find labels
+folders = glob(train_path + '/*')
+
+# Checking image data randomly
+check_random = np.random.choice(train_files)
+plt.imshow(load_img(check_random))
+plt.show()
+
+print(check_random)
